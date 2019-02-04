f27a7da1185517335f9de9897114cada51b6ef4e
pedro <pedroluismontesinosrivera@gmail.com>
2019-02-04 16:21:19 -0500

    api key tested.

--- treasure-hunt/src/App.js
+++ treasure-hunt/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import axios from 'axios';
+const url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/'
+
 class App extends Component {
-  state = {
-    visited_rooms: {
-      0: { n: "?", s: "?", w: "?", e: "?" }
-    }
+  
+  componentDidMount(){
+    this.init();
   }
+
+  init = () => {
+    const config = {
+      headers: {
+        Authorization: `Token ${process.env.REACT_APP_API_KEY}`
+      }
+    }
+    axios
+      .get(url, config)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  };
+
   render() {
     return (
       <div className="App">
+        <h1>MAP!</h1>
       </div>
     );
   }
 }
 
 export default App;
+
+// curl - X GET - H 'Authorization: Token [YOUR_API_KEY]' https://lambda-treasure-hunt.herokuapp.com/api/adv/init/
