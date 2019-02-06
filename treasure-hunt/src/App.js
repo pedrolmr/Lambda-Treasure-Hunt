5e4ab91d52d28059ea8e5378ca22441a7c6b0707
pedro <pedroluismontesinosrivera@gmail.com>
2019-02-06 15:07:49 -0500

    fixing some bugs

--- treasure-hunt/src/App.js
+++ treasure-hunt/src/App.js
@@ -2,33 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 
 import axios from 'axios';
-const url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/'
-
+const url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv';
+const config = {
+  headers: {
+    Authorization: `Token ${process.env.REACT_APP_API_KEY}`
+  }
+}
 class App extends Component {
   state = {
     room_id: 0,
-    coordinates: (60,60),
+    coordinates: {x: 60, y: 60},
     exits: [],
     cooldown: 0,
-    graph: { 0: { "n": "?", "s": "?", "e": "?", "w": "?" } }
-
+    reverseDirection: { "n": "s", "s": "n", "w": "e", "e": "w"},
+    graph: {},
+    items:[],
+    storeInfo: null,
+    curr_room: null,
+    progress:0,
+    path:[]
   }
   componentDidMount(){
-    this.init();
+    if(localStorage.hasOwnProperty('graph')){
+      let localGraph = JSON.parse(localStorage.getItem('graph'));
+      this.setState({graph: localGraph})
+    }
   }
 
-  init = () => {
-    const config = {
-      headers: {
-        Authorization: `Token ${process.env.REACT_APP_API_KEY}`
-      }
-    }
+  movePlayer = direction => {
+    const data = {direction: direction}
     axios
-      .get(url, config)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
-  };
-
+      .post(`${url}/move`, data, config)
+      .then(res => {
+        console.log(res.data)
+        if('room_id' in res.data){
+          this.setState({
+            curr_room: res.room_id,
+            exits: res.exits,
+            coordinates: res.coordinates,
+            cooldown: res.cooldown
+          })
+        }
+      })
+  }
   render() {
     return (
       <div className="App">
