73e14019d31d4ebc4427ab54f3d2937fa249c84b
pedro <pedroluismontesinosrivera@gmail.com>
2019-02-13 03:24:29 -0500

    map visualization done

--- treasure-hunt/src/App.js
+++ treasure-hunt/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import data from './data.json';
 import Map from './components/map';
 import axios from 'axios';
 const url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv';
@@ -33,98 +32,7 @@ class App extends Component {
     visited: new Set(),
     players: []
   }
-
-  // componentDidMount(){
-  //   axios
-  //     .get(`${url}/init`, config)
-  //     .then(res => {
-  //       let graph = this.updateGraph(
-  //         res.data.room_id,
-  //         this.parseCoords(res.data.coordinates),
-  //         res.data.exits
-  //       );
-  //       this.setState({
-  //         room_id: res.data.room_id,
-  //         messages: res.data.messages,
-  //         coordinates:this.parseCoordinates(res.data.coordinates),
-  //         exits: [...res.data.exits],
-  //         cooldown: res.data.cooldown,
-  //         items: [...res.data.items],
-  //         player:[...res.data.players],
-  //         graph
-  //       });
-  //     })
-  //   if (localStorage.hasOwnProperty('graph')) {
-  //     let value = JSON.parse(localStorage.getItem('graph'));
-  //     this.setState({ graph: value });
-  //   } else {
-  //     localStorage.setItem('graph', JSON.stringify(data));
-  //     let value = JSON.parse(localStorage.getItem('graph'));
-  //     this.setState({ graph: value });
-  //   }
-  // }
-
-  // updateGraph = (id, coords, exits) => {
-  //   let graph = Object.assign({}, this.state.graph);
-  //   if (!this.state.graph[id]) {
-  //     let payload = [];
-  //     payload.push(coords);
-  //     const moves = {};
-  //     exits.forEach(exit => {
-  //       moves[exit] = '?';
-  //     });
-  //     payload.push(moves);
-  //     graph = { ...graph, [id]: payload };
-  //   }
-
-  //   localStorage.setItem('graph', JSON.stringify(graph));
-  //   return graph;
-  // };
-
-  // parseCoords = coords => {
-  //   const coordsObject = {};
-  //   const coordsArray = coords.replace(/[{()}]/g, '').split(',');
-
-  //   coordsArray.forEach(coord => {
-  //     coordsObject['x'] = parseInt(coordsArray[0]);
-  //     coordsObject['y'] = parseInt(coordsArray[1]);
-  //   });
-
-  //   return coordsObject;
-  // };
-
-  // movePlayer = direction => {
-  //   const data = { direction: direction }
-  //   axios
-  //     .post(`${url}/move`, data, config)
-  //     .then(res => {
-  //       this.setState({
-  //         curr_room: res.room_id,
-  //         exits: res.exits,
-  //         coordinates: res.coordinates,
-  //         cooldown: res.cooldown
-  //       })
-  //       console.log('curr_room:', this.state.curr_room);
-  //     })
-  // };
-
-  // enterDirection = event => {
-  //   event.preventDefault();
-  //   const move = { direction: this.state.input }
-  //   if (this.state.input === 'n' || this.state.input === 'e' || this.state.input === 'w' || this.state.input === 's'){
-  //     axios
-  //       .post(`${url}/move`, move, config)
-  //       .then(res => {
-  //         this.setState({
-  //           room_id: this.state.room_id,
-  //           coordinates: this.state.coordinates,
-  //           exits: this.state.exits,
-  //           items: this.state.items,
-  //           path: this.state.path.concat(this.state.input)                 
-  //         });
-  //       })
-  //   }
-  // }
+  
   render() {
     return (
       <div className="App">
